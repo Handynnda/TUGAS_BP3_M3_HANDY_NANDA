b81ab72882cd859b36a409783aa32c8dabe4398e
Handy Nanda Fachrizal <Handynandaf@gmail.com>
2024-12-03 17:21:47 +0700

    tugas modul5

--- app/src/main/java/com/example/tugas_bp3_m3_handy_nanda/Dashboard.java
+++ app/src/main/java/com/example/tugas_bp3_m3_handy_nanda/Dashboard.java
@@ -1,24 +1,28 @@
 package com.example.tugas_bp3_m3_handy_nanda;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_designer);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void openWebsite1(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://appmaster.io/id/blog/cara-mendesain-aplikasi"));
+        startActivity(intent);
+    }
+
+    public void openWebsite2(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dewaweb.com/blog/cara-membuat-aplikasi-web/"));
+        startActivity(intent);
     }
 }
